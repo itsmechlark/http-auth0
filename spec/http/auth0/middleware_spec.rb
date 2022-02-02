acc9e1a6c1c09fbb2a453028033994b40d176da7
Kevin Candelaria <kevdashdev@gmail.com>
2022-02-02 14:55:54 +0800

    Fix rubocop violations

--- spec/http/auth0/middleware_spec.rb
+++ spec/http/auth0/middleware_spec.rb
@@ -5,17 +5,17 @@ require "http/auth0/middleware"
 # TODO: Replace with actual spec when middleware feature comes in as
 # these are just to appease Simplecov
 RSpec.describe(HTTP::Auth0::Middleware) do
-  describe '#on_request(env)' do
-    let(:app) { double("app") }
+  describe "#on_request(env)" do
+    let(:app) { instance_double("app") }
     let(:middleware) { described_class.new(app) }
-    let(:request_headers) { { Authorization: 'ALREADY_SET' } }
+    let(:request_headers) { { Authorization: "ALREADY_SET" } }
 
     it do
       expect(
         middleware.on_request(
-          OpenStruct.new(request_headers: request_headers)
+          Struct.new(:request_headers).new(request_headers: request_headers)
         )
-      ).to be_nil
+      ).to(be_nil)
     end
   end
 end
