5cafb6e3aefd1e0a58e094d2cc3b3c1fd30c82b6
Kevin Candelaria <kevdashdev@gmail.com>
2022-02-03 13:55:05 +0800

    Add validation for client_id and client_secret for Auth0.token

--- lib/http/auth0.rb
+++ lib/http/auth0.rb
@@ -4,11 +4,28 @@ require "dry/configurable"
 
 module HTTP
   class Auth0
+    class ConfigurationError < StandardError; end
+
     extend(Dry::Configurable)
 
     setting(:client_id)
     setting(:client_secret)
     setting(:domain)
+
+    class << self
+      def token(aud:)
+        validate_configuration(key: :client_id)
+        validate_configuration(key: :client_secret)
+
+        "IMPLEMENT_ME"
+      end
+
+      private
+
+      def validate_configuration(key:)
+        raise ConfigurationError, "Missing #{key} in configuration" if [nil, ""].any?(config.send(key))
+      end
+    end
   end
 end
 
--- spec/http/auth0_spec.rb
+++ spec/http/auth0_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe(HTTP::Auth0) do
   let(:client_secret) { "CLIENT_SECRET" }
   let(:domain)        { "firstcircle.ph" }
 
+  before { described_class.reset_config }
+
   it "has a version number" do
     expect(HTTP::Auth0::VERSION).to(be_a(String))
   end
@@ -36,6 +38,42 @@ RSpec.describe(HTTP::Auth0) do
       it { expect(described_class.config.client_id).to(eq(client_id)) }
       it { expect(described_class.config.client_secret).to(eq(client_secret)) }
       it { expect(described_class.config.domain).to(eq(domain)) }
+    end
+  end
+
+  describe ".token(aud:)" do
+    let(:aud) { "https://api.firstcircle.ph" }
+
+    before do
+      described_class.config.client_id      = client_id
+      described_class.config.client_secret  = client_secret
+      described_class.config.domain         = domain
+    end
+
+    context "when client_id is nil" do
+      before { described_class.config.client_id = nil }
+
+      it do
+        expect { described_class.token(aud: aud) }.to(raise_error(
+          an_instance_of(HTTP::Auth0::ConfigurationError)
+            .and(having_attributes(message: "Missing client_id in configuration"))
+        ))
+      end
+    end
+
+    context "when client_secret is nil" do
+      before { described_class.config.client_secret = nil }
+
+      it do
+        expect { described_class.token(aud: aud) }.to(raise_error(
+          an_instance_of(HTTP::Auth0::ConfigurationError)
+            .and(having_attributes(message: "Missing client_secret in configuration"))
+        ))
+      end
+    end
+
+    context "when client id and secret are provided" do
+      it { expect(described_class.token(aud: aud)).to(eq("IMPLEMENT_ME")) }
     end
   end
 end
